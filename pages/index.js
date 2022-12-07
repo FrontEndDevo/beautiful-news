2b06aa15f2874f09a54eda2e2d0402f7f322d014
FrontEndDevo <mylinkacco@gmail.com>
2022-12-07 14:19:37 +0200

    Create Header component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Header from "../components/Header/Header";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Header />
+    </>
+  );
 }
