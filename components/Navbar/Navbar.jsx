c4e34928ab0e09ed3c50e5dff1e6a0eb43cb157a
FrontEndDevo <mylinkacco@gmail.com>
2022-12-21 21:15:27 +0200

    Create Navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+import classes from "./Navbar.module.scss";
+const Navbar = () => {
+  return <div className={classes.navbar}>Navbar</div>;
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Header from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Header />
     </>
   );
