8c222185a6fd74466a253d1ccb529750016bc8b9
FrontEndDevo <mylinkacco@gmail.com>
2023-01-23 22:25:49 +0200

    Almost style Navbar component

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,24 +1,32 @@
 import classes from "./Navbar.module.scss";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faMagnifyingGlass,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
     <div className={classes.navbar}>
-      <div className="L-H-S">
+      <div className={classes["L-H-S"]}>
         <div className={classes.title}>
           {/* Here I used (PostImage) tool to create URL for my local image */}
-          <img src="https://i.postimg.cc/1t8Z3bF7/icon.png" alt="" />
-          <h1>
-            <span>Beautiful</span>News
-          </h1>
+          <img src="https://i.postimg.cc/1t8Z3bF7/icon.png" alt="navbar-icon" />
+          <Link href="./">
+            Beautiful<span>News</span>
+          </Link>
         </div>
-        <div className={classes.pages}>
-          <Link href="./">About</Link>
-          <Link href="./">Channels</Link>
-        </div>
+        <ul className={classes.pages}>
+          <li>
+            <Link href="./">About</Link>
+          </li>
+          <li>
+            <Link href="./">Channels</Link>
+          </li>
+        </ul>
       </div>
-      <div className="R-H-S">
+      <div className={classes["R-H-S"]}>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
         <FontAwesomeIcon icon={faBars} />
         {/* <FontAwesomeIcon icon={faXmark} /> */}
