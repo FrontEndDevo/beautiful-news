77cecde856cc42c4e8d76b0ce78096dcf27933c1
FrontEndDevo <mylinkacco@gmail.com>
2023-01-23 21:35:18 +0200

    Writing JSX in Navbar component

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import classes from "./Navbar.module.scss";
-import icon from "../../assets/images/navbar-icon.png";
+import myIcon from '../../assets/images/ico.webp';
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className={classes.navbar}>
-      <img src={icon} alt="" />
-      <h2>Navbar</h2>
+      <div className="L-H-S">
+        <div className={classes.title}>
+          {/* Here I used (PostImage) tool to create URL for my local image */}
+          <img src="https://i.postimg.cc/1t8Z3bF7/icon.png" alt="" />
+          <h1>
+            <span>Beautiful</span>News
+          </h1>
+        </div>
+        <div className={classes.pages}>
+          <Link>About</Link>
+          <Link>Channels</Link>
+        </div>
+      </div>
+      <div className="R-H-S">
+        {/* Search icon //install fontawesome library */}
+        <div className="menu"></div>
+      </div>
     </div>
   );
 };
