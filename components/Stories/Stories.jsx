64944b776cff2fef12f3d8e2aebf614fde2b139b
FrontEndDevo <mylinkacco@gmail.com>
2023-01-30 17:54:25 +0200

    Add DUMMY_NEWS array to Stories component

--- components/Stories/Stories.jsx
+++ components/Stories/Stories.jsx
@@ -1,6 +1,140 @@
 import classes from "./Stories.module.scss";
+const DUMMY_NEWS = [
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+  {
+    source: { id: null, name: "Marketscreener.com" },
+    urlToImage: "https://www.marketscreener.com/images/twitter_MS_fdnoir.png",
+    title: "Ford to Cut Prices, Boost Production of EV Mustang Mach-E",
+    urlToStory:
+      "https://www.marketscreener.com/quote/stock/FORD-MOTOR-COMPANY-12542/news/Ford-to-Cut-Prices-Boost-Production-of-EV-Mustang-Mach-E-42847481/?utm_medium=RSS&utm_content=20230130",
+    author: "MarketScreener",
+  },
+];
 const Stories = () => {
-  return <div className={classes.stories}>News</div>;
+  return (
+    <div className={classes.stories}>
+      <h3>Latest News</h3>
+    </div>
+  );
 };
 
 export default Stories;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
+import Stories from "../components/Stories/Stories";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Stories />
     </>
-  )
+  );
 }
