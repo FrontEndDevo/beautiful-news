d7a6b1bcdea430f8d372afa9c665da7a27a51121
FrontEndDevo <mylinkacco@gmail.com>
2023-01-30 13:29:59 +0200

    Add Global Metadata (Title, Description and favicon-icon) in _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import Head from "next/head";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        // Global Metadata (Title, Description and favicon-icon)
+        <title>Beautiful News</title>
+        <meta
+          name="description"
+          content="Our site includes all types of news like political, judicial, entertainment, sports and more. We present a continuous flow of links to articles organized from thousands of publishers and magazines."
+        />
+        <link
+          rel="shortcut icon"
+          href="https://i.postimg.cc/1t8Z3bF7/icon.png"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Head from "next/head";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
+  <Head>
+    // <meta charset="UTF-8" />
+    // <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    // <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    // <title>Document</title>
+  </Head>
   return (
     <>
       <Navbar />
