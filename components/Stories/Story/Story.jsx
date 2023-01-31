2bc3746321b15d37e80e29ce2e362e4d0becaa56
FrontEndDevo <mylinkacco@gmail.com>
2023-01-31 12:53:28 +0200

    Add icons to Story component from FontAwesome library

--- components/Stories/Story/Story.jsx
+++ components/Stories/Story/Story.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark, faHeart, faPlay } from "@fortawesome/free-solid-svg-icons";
 import classes from "./Story.module.scss";
 const Story = (props) => {
   const { author, title, urlToImage: img } = props;
@@ -7,6 +9,13 @@ const Story = (props) => {
       <p className={classes.title}>{title}</p>
       <img className={classes["story-image"]} src={img} alt={author} />
       <div className={classes["image-hover"]}></div>
+      <div className={classes.icons}>
+        <div>
+          <FontAwesomeIcon icon={faBookmark} />
+          <FontAwesomeIcon icon={faHeart} />
+        </div>
+        <FontAwesomeIcon icon={faPlay} />
+      </div>
     </div>
   );
 };
