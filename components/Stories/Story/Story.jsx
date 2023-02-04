a569481abaf52b0728012ae928529b4849d2c9fc
FrontEndDevo <mylinkacco@gmail.com>
2023-02-04 17:07:09 +0200

    Fix long author name problem by cutting it in Stories component

--- components/Stories/Story/Story.jsx
+++ components/Stories/Story/Story.jsx
@@ -4,9 +4,12 @@ import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import classes from "./Story.module.scss";
 const Story = (props) => {
   const { author, title, urlToImage: img } = props;
+  // Fix (Long author name) problem
+  const editedAuthor =
+    author.length > 20 ? `${author.split(" ")[0]}...` : author;
   return (
     <div className={classes.story}>
-      <h4 className={classes.author}>{author}</h4>
+      <h4 className={classes.author}>{editedAuthor}</h4>
       <p className={classes.title}>{title}</p>
       <img className={classes["story-image"]} src={img} alt={author} />
       <div className={classes["image-hover"]}></div>
