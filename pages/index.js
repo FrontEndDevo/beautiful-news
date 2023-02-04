17a59552a2a60b8aa43ba37f3c661a4d33a68ee3
FrontEndDevo <mylinkacco@gmail.com>
2023-02-04 15:40:26 +0200

    Fetching data from our API (newsapi.org) and handling them in an array in index.js in pages folder

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
 import Stories from "../components/Stories/Stories";
 
-export default function Home() {
+const NEWS_API =
+  "https://newsapi.org/v2/everything?q=tesla&from=2023-01-04&sortBy=publishedAt&apiKey=8804ae5da994436aa3ab963e0217fe73";
+
+export default function Home(props) {
+  console.log(props.news);
   return (
     <>
       <Navbar />
@@ -10,4 +14,33 @@ export default function Home() {
       <Stories />
     </>
   );
+}
+
+export async function getStaticProps() {
+  // Fetching news from our API:
+  const response = await fetch(NEWS_API);
+  const data = await response.json();
+
+  // Handling results and push them to an array.
+  const loadedNews = [];
+  if (data.totalResults > 0) {
+    for (const key in data.articles) {
+      loadedNews.push({
+        key,
+        source: data.articles[key].source,
+        author: data.articles[key].author,
+        title: data.articles[key].title,
+        description: data.articles[key].description,
+        url: data.articles[key].url,
+        urlToImage: data.articles[key].urlToImage,
+        content: data.articles[key].content,
+      });
+    }
+  }
+
+  return {
+    props: {
+      news: loadedNews,
+    },
+  };
 }
