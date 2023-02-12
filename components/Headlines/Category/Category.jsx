49e47d30a24d6a87450f710fb8ea4eb26b8d91c4
FrontEndDevo <mylinkacco@gmail.com>
2023-02-12 16:12:54 +0200

    Initiate Category component and render all our headlines

---
+++ components/Headlines/Category/Category.jsx
@@ -0,0 +1,6 @@
+import styles from "./Category.module.scss";
+const Category = ({ headline }) => {
+  return <li>{headline}</li>;
+};
+
+export default Category;
--- components/Headlines/Headlines.jsx
+++ components/Headlines/Headlines.jsx
@@ -1,13 +1,28 @@
+import Category from "./Category/Category";
 import styles from "./Headlines.module.scss";
+const TOP_HEADLINES = [
+  "general",
+  "entertainment",
+  "business",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
 const Headlines = () => {
+  const allHeadlines = TOP_HEADLINES.map((headline, index) => (
+    <Category key={index} headline={headline} />
+  ));
   return (
     <div className={styles.headlines}>
       <h2>
         Re-framing
         <br />
-        the world
+        <span>the world</span>
       </h2>
-        <div className={styles.categories}></div>
+      <div className={styles.categories}>
+        <ul>{allHeadlines}</ul>
+      </div>
     </div>
   );
 };
