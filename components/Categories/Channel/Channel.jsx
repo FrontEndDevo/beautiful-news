09905b768cd39c1d92c81c0bce32a1f96f0260b7
FrontEndDevo <mylinkacco@gmail.com>
2023-02-15 21:56:26 +0200

    Render incoming props in Channel component

--- components/Categories/Channel/Channel.jsx
+++ components/Categories/Channel/Channel.jsx
@@ -1,14 +1,15 @@
-import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import classes from "./Channel.module.scss";
 const Channel = (props) => {
+  const { headlineName: name, headlineImg: img } = props;
   return (
     <li className={classes.channel}>
-      <Link>
-        <img src="" alt="" />
+      <Link href={name}>
+        <img src={img} alt={name} />
         <div className={classes.words}>
-          <h4></h4>
+          <h4>{name}</h4>
           <div className={classes.watch}>
             <FontAwesomeIcon icon={faEye} />
             <p>Watch</p>
