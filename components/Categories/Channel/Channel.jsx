bef14c4e4497492b8235f4ce7699287e200741b5
FrontEndDevo <mylinkacco@gmail.com>
2023-02-15 21:36:48 +0200

    Initiate the main structure for Channel component

--- components/Categories/Channel/Channel.jsx
+++ components/Categories/Channel/Channel.jsx
@@ -1,6 +1,22 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import classes from "./Channel.module.scss";
 const Channel = (props) => {
-  return <li className={classes.channel}></li>;
+  return (
+    <li className={classes.channel}>
+      <Link>
+        <img src="" alt="" />
+        <div className={classes.words}>
+          <h4></h4>
+          <div className={classes.watch}>
+            <FontAwesomeIcon icon={faEye} />
+            <p>Watch</p>
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default Channel;
