4546b2683aac4bdb7da3f3849bc0f171512e0a09
FrontEndDevo <mylinkacco@gmail.com>
2023-02-19 16:17:19 +0200

    Put the main structure in Filter component

--- components/Filter/Filter.jsx
+++ components/Filter/Filter.jsx
@@ -1,3 +1,5 @@
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./Filter.module.scss";
 const languages = [
   "en",
@@ -21,7 +23,28 @@ const sortBy = ["publishedAt", "relevancy", "popularity"];
 const pageSize = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 const Filter = () => {
-  return <div className={classes.filter}>Filter</div>;
+  return (
+    <div className={classes.filter}>
+      <FontAwesomeIcon icon={faFilter} />
+      <div className={classes.inputs}>
+        <input type="text" name="keyword" id="" />
+        <select className={classes["sort-by"]} name="" id="">
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+        <div className={classes.short}>
+          <select className={classes.languages} name="" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+          <select className={classes.size} name="" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
