b914fcee5b3188cebd8b87c3651d2e1452c9b65a
FrontEndDevo <mylinkacco@gmail.com>
2023-02-19 15:25:06 +0200

    Initiate filtration arrays (sortBy, pageSize, languages)

--- components/Filter/Filter.jsx
+++ components/Filter/Filter.jsx
@@ -1,6 +1,27 @@
 import classes from "./Filter.module.scss";
+const languages = [
+  "en",
+  "ar",
+  "de",
+  "es",
+  "fr",
+  "he",
+  "it",
+  "nl",
+  "no",
+  "pt",
+  "ru",
+  "sv",
+  "ud",
+  "zh",
+];
+
+const sortBy = ["publishedAt", "relevancy", "popularity"];
+
+const pageSize = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
 const Filter = () => {
-  return <div>Filter</div>;
+  return <div className={classes.filter}>Filter</div>;
 };
 
 export default Filter;
