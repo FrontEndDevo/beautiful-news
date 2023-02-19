eb48da429c3f03e122f73dbce2a2eaac8d654334
FrontEndDevo <mylinkacco@gmail.com>
2023-02-19 16:25:58 +0200

    Render all options arrays in Filter component

--- components/Filter/Filter.jsx
+++ components/Filter/Filter.jsx
@@ -1,6 +1,7 @@
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./Filter.module.scss";
+import Options from "./Options/Options";
 const languages = [
   "en",
   "ar",
@@ -23,24 +24,29 @@ const sortBy = ["publishedAt", "relevancy", "popularity"];
 const pageSize = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 const Filter = () => {
-  
+  const renderedLanguages = languages.map((lang) => (
+    <Options key={lang} option={lang} />
+  ));
+  const renderedSortBy = sortBy.map((item) => (
+    <Options key={item} option={item} />
+  ));
+  const renderedPageSize = pageSize.map((size) => (
+    <Options key={size} option={size} />
+  ));
   return (
     <div className={classes.filter}>
       <FontAwesomeIcon icon={faFilter} />
       <div className={classes.inputs}>
         <input type="text" name="keyword" id="" />
         <select className={classes["sort-by"]} name="" id="">
-          <option value="1">1</option>
-          <option value="2">2</option>
+          {renderedSortBy}
         </select>
         <div className={classes.short}>
           <select className={classes.languages} name="" id="">
-            <option value="1">1</option>
-            <option value="2">2</option>
+            {renderedLanguages}
           </select>
           <select className={classes.size} name="" id="">
-            <option value="1">1</option>
-            <option value="2">2</option>
+            {renderedPageSize}
           </select>
         </div>
       </div>
