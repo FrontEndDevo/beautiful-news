1468f142282f8f4dd3f777398ddec900f5c69d39
FrontEndDevo <mylinkacco@gmail.com>
2023-02-19 16:28:10 +0200

    Store all options arrays in only one object in Filter component

--- components/Filter/Filter.jsx
+++ components/Filter/Filter.jsx
@@ -2,35 +2,35 @@ import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./Filter.module.scss";
 import Options from "./Options/Options";
-const languages = [
-  "en",
-  "ar",
-  "de",
-  "es",
-  "fr",
-  "he",
-  "it",
-  "nl",
-  "no",
-  "pt",
-  "ru",
-  "sv",
-  "ud",
-  "zh",
-];
-
-const sortBy = ["publishedAt", "relevancy", "popularity"];
-
-const pageSize = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const optionsObj = {
+  languages: [
+    "en",
+    "ar",
+    "de",
+    "es",
+    "fr",
+    "he",
+    "it",
+    "nl",
+    "no",
+    "pt",
+    "ru",
+    "sv",
+    "ud",
+    "zh",
+  ],
+  sortBy: ["publishedAt", "relevancy", "popularity"],
+  pageSize: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+};
 
 const Filter = () => {
-  const renderedLanguages = languages.map((lang) => (
+  const renderedLanguages = optionsObj.languages.map((lang) => (
     <Options key={lang} option={lang} />
   ));
-  const renderedSortBy = sortBy.map((item) => (
+  const renderedSortBy = optionsObj.sortBy.map((item) => (
     <Options key={item} option={item} />
   ));
-  const renderedPageSize = pageSize.map((size) => (
+  const renderedPageSize = optionsObj.pageSize.map((size) => (
     <Options key={size} option={size} />
   ));
   return (
