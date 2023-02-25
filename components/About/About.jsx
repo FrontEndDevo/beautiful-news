2643a9f5297cae5766d97f213cf7fee627ddcde4
FrontEndDevo <mylinkacco@gmail.com>
2023-02-25 18:04:57 +0200

    Add About description and finish styling

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -15,6 +15,34 @@ const About = () => {
           alt="Beautiful News About"
         />
       </div>
+      <div className={styles.description}>
+        <p>
+          Beautiful News is a content platform that publishes one positive story
+          every day of the year. We are dedicated to unearthing inspiring,
+          uplifting, and authentic voices that reflect the good in humanity, as
+          well as the beauty of destinations and our natural world.
+        </p>
+        <p>
+          Beautiful News launched in South Africa on 1 November 2016. Since
+          inception, we’ve shared positive micro-documentary content at 4:14pm,
+          in recognition of the moment that Nelson Mandela was released from
+          prison. In honour of his legacy, we are reframing our world for the
+          betterment of nature and humankind.
+        </p>
+        <p>
+          We gather our stories by keeping a pulse on individuals and
+          organisations that are making a positive impact through their ideas,
+          actions, initiatives and contributions. This diverse set of
+          inspirational narratives collectively showcases humanity’s inherent
+          goodness.
+        </p>
+        <p>
+          Beautiful News offers a platform to amplify their message because we
+          believe that together, we are stronger. It’s these stories that will
+          unite us as we create a community committed to positive action and
+          change.
+        </p>
+      </div>
     </div>
   );
 };
