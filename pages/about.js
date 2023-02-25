81429c66ad2d66c1b639428597d8627e840520e2
FrontEndDevo <mylinkacco@gmail.com>
2023-02-25 17:37:46 +0200

    Create About page

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Navbar from "../components/Navbar/Navbar";
+const AboutPage = () => {
+  return (
+    <>
+      <Head>
+        <title>About | Beautiful News</title>
+        <meta name="description" content="Read more about Beautiful News" />
+      </Head>
+      <Navbar />
+    </>
+  );
+};
+
+export default about;
