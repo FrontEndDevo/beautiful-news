5ffd68e5d4eccd9d5c83cd3216e210ea96e8b3c0
FrontEndDevo <mylinkacco@gmail.com>
2023-03-05 22:53:02 +0200

    Add both (evething, headlines - slices) to redux-store

--- store/redux-store.js
+++ store/redux-store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import headlinesNewsSlice from "./headlines-slice";
+import everythingNewsSlice from "./everything-slice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    headlines: headlinesNewsSlice.reducer,
+    everything: everythingNewsSlice.reducer,
+  },
 });
 
 export default store;
