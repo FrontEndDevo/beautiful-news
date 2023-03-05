6feb351ecfcd626c9b7bfc1b929706d6fb3f739c
FrontEndDevo <mylinkacco@gmail.com>
2023-03-05 22:36:24 +0200

    Create and initiate (everything-slice) file

---
+++ store/everything-slice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialEverythingState = {
+  totalResults: 0,
+  articles: [],
+};
+
+const everythingNewsSlice = createSlice({
+  name: "everything-news",
+  initialState: initialEverythingState,
+  reducers: {
+    everythingStore: (state, action) => {
+      state.articles = action.payload.news;
+      state.totalResults = action.payload.total;
+    },
+  },
+});
+
+export const everythingNewsActions = everythingNewsSlice.actions;
+
+export default everythingNewsSlice;
