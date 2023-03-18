2b139a96d8622806bfd9d321179425fb54181620
FrontEndDevo <mylinkacco@gmail.com>
2023-03-19 01:35:14 +0200

    Detect which story was clicked in any page in [chosenStory].js

--- pages/news/[ChosenStory].js
+++ pages/news/[ChosenStory].js
@@ -1,4 +1,19 @@
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 const ChosenStory = () => {
+  const router = useRouter();
+  // Fetching all news from redux-store:
+  const headlines = useSelector((state) => state.headlines.headlines);
+  const everything = useSelector((state) => state.everything.articles);
+
+  // Catch the clicked story using (high-ordered-function (FILTER)):
+  const catchedStory = [...headlines[0].articles, ...everything].filter(
+    (chosenOne) =>
+      chosenOne.title.replace(/\s/g, "-") == router.query.ChosenStory
+  )[0];
+
+  console.log(catchedStory);
+
   return <div>ChosenStory</div>;
 };
 
