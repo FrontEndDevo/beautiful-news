f6cda04fde097f61234279995a70aa0be62572e5
FrontEndDevo <mylinkacco@gmail.com>
2023-03-19 01:05:51 +0200

    Create new dynamic page for desplaying each story info. ([chosenStory].js) in pages folder

--- pages/news/[ChosenStory].js
+++ pages/news/[ChosenStory].js
@@ -1,8 +1,4 @@
-import { useRouter } from "next/router";
 const ChosenStory = () => {
-  const router = useRouter();
-  console.log(router.query.ChosenStory);
-
   return <div>ChosenStory</div>;
 };
 
