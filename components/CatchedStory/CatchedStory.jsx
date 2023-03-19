db865d30a2c553deeb7f9e929405701ef3d046ab
FrontEndDevo <mylinkacco@gmail.com>
2023-03-19 19:06:45 +0200

    Almost finish styles of CatchedStory component

--- components/CatchedStory/CatchedStory.jsx
+++ components/CatchedStory/CatchedStory.jsx
@@ -24,13 +24,12 @@ const CatchedStory = (props) => {
     <div className={classes["catched-story"]}>
       <div className={classes["top-content"]}>
         <h2 className={classes.title}>{title}</h2>
-        <h4 className={classes.author}>{author}</h4>
         <div className={classes.links}>
           <Link href="/">General</Link>
           <Link href="/everything">Everything</Link>
         </div>
         <div className={classes.social}>
-          <Link href={url} target="_blank">
+          <Link className={classes.play} href={url} target="_blank">
             <FontAwesomeIcon icon={faPlay} />
           </Link>
           <div className={classes.icons}>
@@ -40,12 +39,15 @@ const CatchedStory = (props) => {
             <FontAwesomeIcon icon={faHeart} />
           </div>
         </div>
-        <img className={classes.image} src={urlToImage} alt={author} />
       </div>
+      <h4 className={classes.author}>{author}</h4>
+      <img className={classes.image} src={urlToImage} alt={author} />
       <div className={classes["bottom-content"]}>
-        <p className={classes.content}>{content}</p>
-        <p className={classes.description}>{description}</p>
-        <Link className={classes['read-more']} href={url} target="_blank">
+        <div className={classes.paragraph}>
+          <p className={classes.content}>{content}</p>
+          <p className={classes.description}>{description}</p>
+        </div>
+        <Link className={classes["read-more"]} href={url} target="_blank">
           Read More
         </Link>
       </div>
