7825f8db46bb7745d037d325d2527998dea20309
FrontEndDevo <mylinkacco@gmail.com>
2023-03-22 16:00:25 +0200

    Create 'contact' page in pages folder

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+const ContactUs = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default ContactUs;
