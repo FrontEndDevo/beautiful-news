046a104bbb87a7a1314658a8e45353d79b983b77
FrontEndDevo <mylinkacco@gmail.com>
2023-03-22 16:02:39 +0200

    Create ContactForm component

---
+++ components/ContactForm/ContactForm.jsx
@@ -0,0 +1,7 @@
+ContactForm;
+import classes from "./ContactForm.module.scss";
+const ContactForm = () => {
+  return <div className={classes.contact}>ContactForm</div>;
+};
+
+export default ContactForm;
