2008dc30b2b1863cfeecc497e8baffdac5b9f3fc
FrontEndDevo <mylinkacco@gmail.com>
2023-03-22 16:39:15 +0200

    Finish JSX of ContactForm component

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -1,7 +1,52 @@
-ContactForm;
+import Link from "next/link";
 import classes from "./ContactForm.module.scss";
 const ContactForm = () => {
-  return <div className={classes.contact}>ContactForm</div>;
+  return (
+    <div className={classes.contact}>
+      <div className={classes.text}>
+        <h2>contact beautiful news</h2>
+        <p>
+          If you have a question, comment, or suggestion, please <br /> send us
+          a message we'd love to hear from you.
+        </p>
+      </div>
+      <form>
+        <div>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="My name is"
+            required
+          />
+          <select name="reson-for-contact" id="reson" required>
+            <option value="I am interested in">I am interested in</option>
+            <option value="career opportunities">career opportunities</option>
+            <option value="collaboration">collaboration</option>
+            <option value="other">other</option>
+          </select>
+          <input type="email" name="" id="" placeholder="Email" required />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Your message (optional)"
+          />
+        </div>
+        <div>
+          <button>Send</button>
+          <div>
+            <input type="checkbox" name="" id="" required />
+            <p>
+              I agree with the{" "}
+              <Link href="terms of use">Terms and Conditions</Link>
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import ContactForm from "../components/ContactForm/ContactForm";
 const ContactUs = () => {
   return (
     <>
       <Navbar />
+      <ContactForm />
       <Footer />
     </>
   );
