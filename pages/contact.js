53aec8cfa39436a6dcb764d070e82d8f470eab10
FrontEndDevo <mylinkacco@gmail.com>
2023-03-23 15:25:57 +0200

    Add Head component from nextjs to contact.js page

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,17 @@
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import ContactForm from "../components/ContactForm/ContactForm";
+import Head from "next/head";
 const ContactUs = () => {
   return (
     <>
+      <Head>
+        <title>Contact Us | Beautiful News</title>
+        <meta
+          name="description"
+          content="You can contact us if you have a problem, suggestion or complaint."
+        />
+      </Head>
       <Navbar />
       <ContactForm />
       <Footer />
