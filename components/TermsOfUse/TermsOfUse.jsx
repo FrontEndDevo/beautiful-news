311ef7cdf2244f86b4e961d69b8fda9fdc189969
FrontEndDevo <mylinkacco@gmail.com>
2023-03-29 15:25:11 +0200

    Create (terms of use) component in components folder and initiate it

---
+++ components/TermsOfUse/TermsOfUse.jsx
@@ -0,0 +1,6 @@
+import classes from "./TermsOfUse.module.scss";
+const TermsOfUse = () => {
+  return <div className={classes.terms}>TermsOfUse</div>;
+};
+
+export default TermsOfUse;
--- pages/terms-use.js
+++ pages/terms-use.js
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import TermsOfUse from "../components/TermsOfUse/TermsOfUse";
 const Terms = () => {
   return (
     <>
       <Navbar />
-    
+      <TermsOfUse />
       <Footer />
     </>
   );
