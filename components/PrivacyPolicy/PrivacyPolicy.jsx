86f234567eb8ae2cf89527e2f3e753ae48d4e94c
FrontEndDevo <mylinkacco@gmail.com>
2023-04-06 17:42:35 +0200

    Finish the JSX for PrivacyPolicy component

--- components/PrivacyPolicy/PrivacyPolicy.jsx
+++ components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,6 +1,370 @@
 import classes from "./PrivacyPolicy.module.scss";
 const PrivacyPolicy = () => {
-  return <div className={classes["privacy-policy"]}></div>;
+  return (
+    <div className={classes["privacy-policy"]}>
+      <h2>Privacy Policy</h2>
+      <div>
+        <h3>GINKGO AGENCY (PTY) LTD (“BEAUTIFUL NEWS”)</h3>
+        <p>
+          This Privacy Policy should be read together with our Website Terms and
+          Conditions. All terms and conditions, including defined terms, of our
+          Website Terms and Conditions, are incorporated by reference into this
+          Privacy Policy and have the same meaning unless the context indicates
+          otherwise. In the event of any conflict between any terms in our
+          Website Terms and Conditions and the terms of this Privacy Policy,
+          this Privacy Policy shall prevail.
+        </p>
+        <p>
+          “Personal Information” means any information that we have about you
+          and that identifies or relates specifically to you as an individual.
+        </p>
+      </div>
+      <ul className={classes.order}>
+        <li>
+          <div>
+            <h3>General Terms</h3>
+            <p>
+              BEAUTIFUL NEWS receives Personal Information from Users who access
+              the Website. BEAUTIFUL NEWS makes every effort to protect Personal
+              Information received by it, and in this regard, the Privacy Policy
+              set out below applies.
+            </p>
+            <p>
+              Despite all security measures taken, it is possible for
+              Internet-based communications to be intercepted. Without the use
+              of encryption, the Internet is not a secure medium and privacy
+              cannot be ensured. Internet e-mail is vulnerable and susceptible
+              to interception and forging. BEAUTIFUL NEWS will not be
+              responsible for any damages the User or any third party may suffer
+              as a result of the transmission of confidential or personal
+              information that the User submits to BEAUTIFUL NEWS through the
+              Internet, or that the User expressly or implicitly authorises
+              BEAUTIFUL NEWS to receive, or for any errors or any changes made
+              to any transmitted information.
+            </p>
+            <p>
+              We are committed to the protection of your privacy and will take
+              all reasonable steps to protect any Personal Information that you
+              may provide us with from time to time via the Website or any other
+              channel or medium. Our Privacy Policy indicates what we do with
+              your Personal Information and what you can do to protect that
+              information.
+            </p>
+            <p>
+              Where you submit Personal Information to us via our Website, we
+              may store and use such information for the purposes that you
+              provided it.
+            </p>
+            <p>
+              We will take reasonable measures to delete Personal Information
+              that has become obsolete. Over the period that your Personal
+              Information is being used and for a period of at least one year
+              thereafter, or such longer period as we may be required or
+              entitled in terms of any law, we will keep a record of your
+              Personal Information and the purpose for which it is stored.
+            </p>
+            <p>
+              Should you so request, in writing, we will delete your Personal
+              Information save where we are required or entitled to preserve
+              same in terms of any law.
+            </p>
+            <p>
+              You acknowledge and accept that we cannot take any responsibility
+              for any of your Personal Information that is collected outside the
+              scope of this agreement by any intermediary or other third party.
+            </p>
+            <p>
+              You give permission that if there is a change of ownership at
+              BEAUTIFUL NEWS, then your Personal Information can be transferred
+              to the new owner, provided that the new owner will not dilute your
+              rights and obligations regarding the Personal Information as set
+              out in this privacy policy that had been collected before such
+              transfer.
+            </p>
+            <p>
+              Your Personal Information will not be sold, divulged or made
+              available to any third party except as mentioned in this policy
+              and unless required or permitted by law.
+            </p>
+            <p>You as the User hereby acknowledge and agree as follows:</p>
+            <ul>
+              <li>
+                you authorize BEAUTIFUL NEWS to retain your Personal Information
+                records for as long as it is reasonably necessary;
+              </li>
+              <li>
+                you relieve BEAUTIFUL NEWS of the duty to inform you each time
+                of the fact that your Personal Information is being collected
+                and whether the provision of this information is voluntary or
+                mandatory and what law requires such collection; and
+              </li>
+              <li>
+                if any of your Personal Information which is sought to be
+                processed falls within the definition of ‘special personal
+                information’ as defined by any law, you hereby give your consent
+                to the processing of such information.
+              </li>
+            </ul>
+            <p>
+              The data that we collect from you may be transferred to, and
+              stored at, a destination outside South Africa or the country in
+              which you are located. It may also be processed by staff operating
+              outside South Africa or the country in which you are located. This
+              includes staff engaged in, among other things, the fulfilment of
+              your contact requests, the processing of your contact details and
+              the provision of support services. By submitting your Personal
+              Information, you agree to this transfer, storing or processing.
+              BEAUTIFUL NEWS will take all steps reasonably necessary to ensure
+              that your data is treated securely and in accordance with this
+              Privacy Policy.
+            </p>
+            <p>
+              All information you provide to us is stored on secure servers. Any
+              information will be encrypted using SSL technology. Where we have
+              given you (or where you have chosen) a password which enables you
+              to access certain parts of our site, you are responsible for
+              keeping this password confidential. We ask you not to share a
+              password with anyone.
+            </p>
+            <p>
+              Our site may, from time to time, contain links to and from the
+              websites of our content creation, development and distribution
+              networks and their affiliates. If you follow a link to any of
+              these websites, please note that these websites have their own
+              privacy policies and that we do not accept any responsibility or
+              liability for these policies. Please check these policies before
+              you submit any personal data to these websites.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>Information we collect from you</h3>
+            <p>
+              The Personal Information that we collect and process about you
+              includes the following:
+            </p>
+            <ul>
+              <li>
+                Information you give us. This is information about you that you
+                give us by filling in forms on our Website or by corresponding
+                with us by phone, e-mail or otherwise. It includes information
+                you provide when you engage with content, subscribe to our
+                products and services, search for information, participate in
+                discussion boards or other social media functions on our
+                Website, enter a competition, promotion or survey, and when you
+                report a problem with our Website. The information you give us
+                may include but is not limited to your name, address, e-mail
+                address and phone number, personal description and photographs.
+              </li>
+              <li>
+                Information we collect about you. With regard to each of your
+                visits to our Website we will automatically collect the
+                following information:
+                <ul>
+                  <li>
+                    technical information, including your IP address, browser
+                    type and version, time zone setting, browser plug-in types
+                    and versions, operating system and platform;
+                  </li>
+                  <li>
+                    information about your visit, including the full URLs,
+                    clickstream to, through and from our Website (including date
+                    and time), content you viewed or searched for, page response
+                    times, errors, length of visits to certain pages, page
+                    interaction information (such as scrolling, clicks, and
+                    mouse-overs), methods used to browse away from the page, and
+                    any phone number used to call our customer service number.
+                  </li>
+                  <li>
+                    information we receive from Google Analytics Demographics
+                    and Interest reports combined with information we receive
+                    from your Google profile that you have enabled in your
+                    Google account. The policy that you have accepted in your
+                    Google account (located at
+                    https://www.google.com/policies/technologies/ads/) governs
+                    the data you share with Google and with us.
+                  </li>
+                  <li>
+                    Information we receive from other sources. This includes
+                    data from social media profiles. We also work closely with
+                    other third parties (including, for example, business
+                    partners, sub-contractors in technical services, analytics
+                    providers, search information providers).
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>What we do with your personal information</h3>
+            <p>We use your Personal Information in the following ways:</p>
+            <ul>
+              <li>
+                Information you give to us. We use this information:
+                <ul>
+                  <li>
+                    to carry out our obligations arising from any contracts
+                    entered into between you and us and to provide you with the
+                    information, products and services that you request from us;
+                  </li>
+                  <li>
+                    to provide you with information about other products and
+                    services we offer that are similar to those that you have
+                    already purchased or enquired about;
+                  </li>
+                  <li>
+                    to provide you with, information about products and services
+                    we feel may interest you. If you are a new customer we will
+                    only contact you if you have consented to this. If you do
+                    not want us to use your Personal Information in this way,
+                    please indicate this on the form we use to collect your
+                    Personal Information;
+                  </li>
+                  <li>
+                    to notify you about changes to our products and services;
+                    and
+                  </li>
+                  <li>
+                    to ensure that content from our Website is presented in the
+                    most effective manner.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Information we collect about you. We will use this information:
+                <ul>
+                  <li>
+                    to administer our Website and for internal operations,
+                    including troubleshooting, data analysis, testing, research,
+                    statistical and survey purposes;
+                  </li>
+                  <li>
+                    to improve our Website to ensure that content is presented
+                    in the most effective manner for you and for your computer;
+                  </li>
+                  <li>
+                    to allow you to participate in interactive features of our
+                    products and services, when you choose to do so;
+                  </li>
+                  <li>
+                    as part of our efforts to keep our Website safe and secure;
+                  </li>
+                  <li>
+                    to measure or understand the effectiveness of content we
+                    serve to you and others, and to deliver relevant content to
+                    you;
+                  </li>
+                  <li>
+                    to make suggestions and recommendations to you and other
+                    users of our Website about products or services that may
+                    interest you or them.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Information we receive from other sources. We will combine this
+                information with information you give to us and information we
+                collect about you. We will use this information and the combined
+                information for the purposes set out above (depending on the
+                types of information we receive).
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>Disclosure of your information</h3>
+            <p>
+              You agree that we have the right to share your Personal
+              Information with:
+            </p>
+            <ul>
+              <li>
+                Any member of our group, which means our subsidiaries, our
+                ultimate holding company and its subsidiaries, or any company
+                under common control.
+              </li>
+              <li>
+                Selected third parties including:
+                <ul>
+                  <li>
+                    business partners, suppliers and sub-contractors for the
+                    performance of any contract we enter into with them or you;
+                  </li>
+                  <li>
+                    content creation, development and distribution partner
+                    networks that require the data to select and serve relevant
+                    content to you and others. We do not disclose information
+                    about identifiable individuals to our content creation,
+                    development and distribution partner, but we will provide
+                    them with aggregate information about our users. We may also
+                    use such aggregate information to help content creation,
+                    development and distribution partners reach the kind of
+                    audience they want to target; and
+                  </li>
+                </ul>
+              </li>
+              <p>
+                We will disclose your Personal Information to third parties:
+              </p>
+              <li>
+                In the event that we sell or buy any business or assets, in
+                which case we will disclose your Personal Information to the
+                prospective seller or buyer of such business or assets.
+              </li>
+              <li>
+                If BEAUTIFUL NEWS or substantially all of its assets are
+                acquired by, or merged with, a third party, in which case
+                personal data held by it about its customers will be one of the
+                transferred assets.
+              </li>
+              <li>
+                If we are under a duty to disclose or share your personal data
+                in order to comply with any legal obligation, or in order to
+                enforce or apply our Website Terms and Conditions or any
+                applicable End-User Terms and Conditions and other agreements;
+                or to protect the rights, property, or safety of BEAUTIFUL NEWS,
+                our customers, or others. This includes exchanging information
+                with other companies and organisations for the purposes of fraud
+                protection and credit risk reduction.
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>Changes to our privacy policy</h3>
+            <p>
+              This Privacy Policy may be updated from time to time. Please check
+              back frequently to see any updates or changes.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>Cookies</h3>
+            <p>
+              Our Website may make use of electronic cookies and web analytics
+              tools to optimise page loading speeds, record Website visitor
+              preferences, monitor the success of advertising campaigns and
+              enhance our Website’s overall usability. “Cookies” are files that
+              are stored by your browser on your computer’s hard drive and can
+              be deleted by clearing your browser cache memory. Cookies may be
+              used, for example, to limit the amount of times you see a
+              particular advert or to help remember your personal settings and
+              preferences. If you block cookies from being stored on your
+              computer, the functionality of the service may be negatively
+              affected. For further information, please see our more detailed
+              Cookie Policy.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default PrivacyPolicy;
