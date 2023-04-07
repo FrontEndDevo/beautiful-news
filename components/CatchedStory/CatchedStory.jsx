c0ea556fcd2fb4ac30c7a1200b77b498610d415c
FrontEndDevo <mylinkacco@gmail.com>
2023-04-07 17:11:06 +0200

    Change the logic of destructuring story properties in CatchedStory.jsx (to fix 'Cannot read properties of undefined')

--- components/CatchedStory/CatchedStory.jsx
+++ components/CatchedStory/CatchedStory.jsx
@@ -9,45 +9,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import classes from "./CatchedStory.module.scss";
 import { useEffect, useState } from "react";
-const CatchedStory = (props) => {
+const CatchedStory = ({ story }) => {
   const [authorWidth, setAuthorWidth] = useState("");
 
-  // Extract all properties using destructuring:
-  const {
-    title,
-    author,
-    content,
-    description,
-    urlToImage,
-    url,
-    source: { id, name },
-  } = props.story;
+  // Extract all properties and put default values.
+  const theStory = {
+    title: story.title || "There is no title.",
+    author: story.author || "Unknown author",
+    content: story.content || "There is no content.",
+    description: story.description || "There is no description.",
+    urlToImage:
+      story.urlToImage ||
+      "https://www.linkpicture.com/q/image-not-found-1-scaled.png",
+    url: story.url || "/",
+  };
 
-  // To delete (content… '[+5450 chars]') of each content.
-  const modifiedConetent = content.substring(
-    0,
-    content.split("").lastIndexOf("[")
-  );
+  // Delete (content… '[+5450 chars]') if the content value is not null, otherwise use the default value of content.
+  const modifiedConetent = story.content
+    ? theStory.content.substring(0, theStory.content.split("").lastIndexOf("["))
+    : theStory.content;
 
   useEffect(() => {
     // Ready code to measure word length from (geeksforgeeks):
     // To center the author name on the img below.
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
-    const countAuthorWidth = Math.ceil(context.measureText(author).width);
+    const countAuthorWidth = Math.ceil(
+      context.measureText(theStory.author).width
+    );
     setAuthorWidth(countAuthorWidth);
-  }, [author]);
+  }, [theStory.author]);
 
   return (
     <section className={classes["catched-story"]}>
       <div className={classes["top-content"]}>
-        <h2 className={classes.title}>{title}</h2>
+        <h2 className={classes.title}>{theStory.title}</h2>
         <div className={classes.links}>
           <Link href="/">General</Link>
           <Link href="/everything">Everything</Link>
         </div>
         <div className={classes.social}>
-          <Link className={classes.play} href={url} target="_blank">
+          <Link className={classes.play} href={theStory.url} target="_blank">
             <FontAwesomeIcon icon={faPlay} />
           </Link>
           <div className={classes.icons}>
@@ -59,22 +61,26 @@ const CatchedStory = (props) => {
         </div>
       </div>
       <div className={classes.image}>
-        <img src={urlToImage} alt={author} />
-        {author && (
+        <img src={theStory.urlToImage} alt={theStory.author} />
+        {theStory.author && (
           <h4
             className={classes.author}
             style={{ left: `calc(45% - ${authorWidth}px)` }}
           >
-            {author}
+            {theStory.author}
           </h4>
         )}
       </div>
       <div className={classes["bottom-content"]}>
         <div className={classes.paragraph}>
           <p className={classes.content}>{modifiedConetent}</p>
-          <p className={classes.description}>{description}</p>
+          <p className={classes.description}>{theStory.description}</p>
         </div>
-        <Link className={classes["read-more"]} href={url} target="_blank">
+        <Link
+          className={classes["read-more"]}
+          href={theStory.url}
+          target="_blank"
+        >
           Read More
         </Link>
       </div>
