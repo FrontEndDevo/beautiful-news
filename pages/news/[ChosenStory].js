ef5413719cfd9f66f23aa48dde05261fde6ff89a
FrontEndDevo <mylinkacco@gmail.com>
2023-04-10 15:49:35 +0200

    Change the entire logic in [ChosenStory].js to solve (can't read properties of undefined...)

--- pages/news/[ChosenStory].js
+++ pages/news/[ChosenStory].js
@@ -4,40 +4,68 @@ import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import Head from "next/head";
 import Footer from "../../components/Footer/Footer";
+import { useEffect, useState } from "react";
 const ChosenStory = () => {
+  const [finalStory, setFinalStory] = useState({});
+
   // Fetching all news from redux-store:
-  const headlines = useSelector((state) => state.headlines.headlines);
   const everything = useSelector((state) => state.everything);
+
+  /*
+  First headlines: Our reducer function that subscribed to the Store.
+  Second headlines: Our headlines-array that holds all different categories.
+  We use "Map" to extract each articles-array of different categories.
+  */
+  const headlines = useSelector((state) => state.headlines.headlines).map(
+    (item) => item.articles
+  );
+
+  // Descover if We only have one array for one category (e.g sports) or more than one array for different categories (e.g sports, general)
+  const newHeadlinesArr = [];
+  const newHeadlines =
+    headlines.length == 1
+      ? headlines[0]
+      : headlines.map((headlineArr) => newHeadlinesArr.push(...headlineArr));
+
+  const finalHeadlines =
+    newHeadlinesArr.length > 0 ? newHeadlinesArr : newHeadlines;
 
   const router = useRouter();
 
   // Catch the clicked story using (high-ordered-function (FILTER)):
-  const catchedStory = [
-    ...headlines[0].articles,
-    ...everything.articles,
-  ].filter(
-    (chosenOne) =>
-      chosenOne.title.replace(/[^a-zA-Z0-9 ]/g, "").replace(/\s/g, "-") ==
-      router.query.chosenStory
-  )[0];
+  useEffect(() => {
+    const fetchTheStory = async () => {
+      const catchedStory = await [
+        ...finalHeadlines,
+        ...everything.articles,
+      ].filter(
+        (chosenOne) =>
+          chosenOne.title.replace(/[^a-zA-Z0-9 ]/g, "").replace(/\s/g, "-") ==
+          router.query.chosenStory
+      )[0];
+
+      setFinalStory(catchedStory);
+      console.log(catchedStory);
+    };
+
+    fetchTheStory();
+  }, [headlines, everything, router.query]);
 
   return (
     <>
       <Head>
         <title>{`${
-          catchedStory && catchedStory.title ? catchedStory.title : "Title"
+          finalStory && finalStory.title ? finalStory.title : "No Title"
         } | News | Beautiful News`}</title>
         <meta
           name="description"
           content={`${
-            catchedStory && catchedStory.content
-              ? catchedStory.content
-              : "Content"
+            finalStory && finalStory.content ? finalStory.content : "No Content"
           } | News section | Beautiful News`}
         />
       </Head>
       <Navbar />
-      {catchedStory && <CatchedStory story={catchedStory} />}
+      {finalStory && <CatchedStory story={finalStory} />}
       <Footer />
     </>
   );
