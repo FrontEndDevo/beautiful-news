fdb6d2f46120276e4784e34288fce08db198b66b
FrontEndDevo <mylinkacco@gmail.com>
2023-04-19 00:56:25 +0200

    Finish JSX and styling of CookiePolicy component (Completed)

--- components/CookiePolicy/CookiePolicy.jsx
+++ components/CookiePolicy/CookiePolicy.jsx
@@ -1,6 +1,74 @@
 import classes from "./CookiePolicy.module.scss";
 const CookiePolicy = () => {
-  return <div className={classes.cookies}>CookiePolicy</div>;
+  return (
+    <div className={classes.cookies}>
+      <h2>Cookie policy</h2>
+      <div>
+        <h3>GINKGO AGENCY (PTY) LTD (“BEAUTIFUL NEWS”)</h3>
+        <p>
+          This Cookie Policy should be read together with our Website Terms and
+          Conditions and our Privacy Policy. All terms and conditions, including
+          defined terms, of our Website Terms and Conditions and of our Privacy
+          Policy are incorporated by reference into this Cookie Policy and have
+          the same meaning unless the context indicates otherwise. In the event
+          of any conflict between any terms in our Website Terms and Conditions
+          or our Privacy Policy, this Cookie Policy shall prevail.
+        </p>
+      </div>
+      <div>
+        <h3>Information about our use of cookies</h3>
+        <p>
+          <p>
+            Our Website uses cookies to distinguish you from other Users of our
+            Website. This helps us to provide you with a good experience when
+            you browse our Website and also allows us to improve our Website. By
+            continuing to browse the Website, you are agreeing to our use of
+            cookies.
+          </p>
+          <p>
+            A cookie is a small file of letters and numbers that we store on
+            your browser or the hard drive of your computer if you agree.
+            Cookies contain information that is transferred to your computer's
+            hard drive.
+          </p>
+          <p>We use the following cookies:</p>
+          <ul>
+            <li>
+              Strictly necessary cookies. These are cookies that are required
+              for the operation of our Website. They include, for example,
+              cookies that enable you to log into secure areas of our Website or
+              purchase products or services on our Website.
+            </li>
+            <li>
+              Analytical/performance cookies. They allow us to recognise and
+              count the number of visitors and to see how visitors move around
+              our Website when they are using it. This helps us to improve the
+              way our Website works, for example, by ensuring that Users are
+              finding what they are looking for easily.
+            </li>
+            <li>
+              Functionality cookies. These are used to recognise you when you
+              return to our Website. This enables us to personalise our content
+              for you, greet you by name and remember your preferences (for
+              example, your choice of language or region).
+            </li>
+            <li>
+              Targeting cookies. These cookies record your visit to our Website,
+              the pages you have visited and the links you have followed.
+            </li>
+          </ul>
+          <p>
+            Please note that third parties (including, for example, content
+            creation, development and distribution partner networks and
+            providers of external services like web traffic analysis services)
+            may also use cookies, over which we have no control. These cookies
+            are likely to be analytical/performance cookies or targeting
+            cookies.
+          </p>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CookiePolicy;
