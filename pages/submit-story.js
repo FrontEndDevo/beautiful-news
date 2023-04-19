74c069f43e688007fc3f266962195a8a35e2af52
FrontEndDevo <mylinkacco@gmail.com>
2023-04-19 14:24:28 +0200

    Create submit a story page in pages folder

---
+++ pages/submit-story.js
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Head from "next/head";
+const SubmitStory = () => {
+  return (
+    <>
+      <Head>
+        <title>SUBMIT YOUR STORY | Beautiful News</title>
+        <meta
+          name="description"
+          content="Submit your story now, We want to hear from you. If you have a positive, uplifting or inspiring story to share about yourself or someone you know, let us know using the form below. We’ll be in touch."
+        />
+      </Head>
+      <Navbar />
+
+      <Footer />
+    </>
+  );
+};
+
+export default SubmitStory;
