ccf9550bf9e97c975432cf31cb2c182848eb58c7
FrontEndDevo <mylinkacco@gmail.com>
2023-04-19 18:19:05 +0200

    Install (react-phone-number-input) package + Add text div (title & paragraph) to SubmitStoryForm component and styling it

--- components/SubmitStoryForm/SubmitStoryForm.jsx
+++ components/SubmitStoryForm/SubmitStoryForm.jsx
@@ -1,6 +1,18 @@
 import classes from "./SubmitStoryForm.module.scss";
 const SubmitStoryForm = () => {
-  return <div className={classes["submit-story"]}>SubmitStoryForm</div>;
+  return (
+    <div className={classes["submit-story"]}>
+      <div className={classes.text}>
+        <h2>Submit your story</h2>
+        <p>
+          We want to hear from you. If you have a positive,
+          <br /> uplifting or inspiring story to share about yourself or
+          <br /> someone you know, let us know using the form below.
+          <br /> We’ll be in touch.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SubmitStoryForm;
