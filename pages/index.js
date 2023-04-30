531b1b9483f28c8c625ee3bf39027ff78918aa80
FrontEndDevo <mylinkacco@gmail.com>
2023-05-01 00:20:50 +0300

    fetch all the different kinds of news that we have when the app is starting in index.js (taking a different way for our app)

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import Headlines from "../components/Headlines/Headlines";
+import Headlines, { TOP_HEADLINES } from "../components/Headlines/Headlines";
 import Inbox from "../components/Inbox/Inbox";
 import Navbar from "../components/Navbar/Navbar";
 import Stories from "../components/Stories/Stories";
 import { headlinesActions } from "../store/headlines-slice";
 
-export default function Home(props) {
-  // Store (General-News) in headlines-slice.js
+export default function Home({ allHeadlinesNews }) {
+  // Store different types of our News in headlines-slice store:
   const dispatch = useDispatch();
-  dispatch(
-    headlinesActions.headlineStore({
-      category: "general",
-      news: props.news,
-      total: props.totalResults,
-    })
-  );
+  allHeadlinesNews.map((headline) => {
+    dispatch(headlinesActions.headlineStore(headline));
+  });
   return (
     <>
       <Navbar />
@@ -30,35 +25,42 @@ export default function Home(props) {
   );
 }
 
-// Top-Headlines for (general) category.
-const defaultAPI =
-  "https://newsapi.org/v2/top-headlines?category=general&country=us&pageSize=100&apiKey=8804ae5da994436aa3ab963e0217fe73";
-
 export async function getStaticProps() {
-  // Fetching news from our API:
-  const response = await fetch(defaultAPI);
-  const data = await response.json();
+  const allHeadlinesNewsPromises = TOP_HEADLINES.map(async (category) => {
+    // Fetching different types of news from our API:
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?category=${category.headline}&country=us&pageSize=100&apiKey=8804ae5da994436aa3ab963e0217fe73`
+    );
+    const data = await response.json();
 
-  // Handling results and push them to an array.
-  const loadedNews = [];
-  if (data.totalResults > 0) {
-    for (const key in data.articles) {
-      loadedNews.push({
-        source: data.articles[key].source,
-        author: data.articles[key].author,
-        title: data.articles[key].title,
-        description: data.articles[key].description,
-        url: data.articles[key].url,
-        urlToImage: data.articles[key].urlToImage,
-        content: data.articles[key].content,
-      });
+    // Handling results and push them to an array.
+    const loadedNews = [];
+    if (data.totalResults > 0) {
+      for (const key in data.articles) {
+        loadedNews.push({
+          source: data.articles[key].source,
+          author: data.articles[key].author,
+          title: data.articles[key].title,
+          description: data.articles[key].description,
+          url: data.articles[key].url,
+          urlToImage: data.articles[key].urlToImage,
+          content: data.articles[key].content,
+        });
+      }
     }
-  }
+
+    return {
+      category: category.headline,
+      news: loadedNews,
+      total: data.totalResults,
+    };
+  });
+
+  const allHeadlinesNews = await Promise.all(allHeadlinesNewsPromises);
 
   return {
     props: {
-      news: loadedNews,
-      totalResults: data.totalResults,
+      allHeadlinesNews,
     },
     revalidate: 43200, // Will fetching new 'news' every 12 hours.
   };
