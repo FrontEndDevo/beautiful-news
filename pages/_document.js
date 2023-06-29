3e3a887c94eb51f8e0bf908c64a0a7bb965dc22f
FrontEndDevo <mylinkacco@gmail.com>
2023-06-29 18:02:50 +0300

    Add _document.js file to pages folder to fix a problem related to viewport meta tag in Head component

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+export default Document;
