7998d4268542f0b85b80e9ae7d6bb45f5a3e1c8e
FrontEndDevo <mylinkacco@gmail.com>
2023-07-20 21:05:47 +0300

    Create and initiate a new page (become-contributer) and set Head component with Nabvar and Footer components

---
+++ pages/become-contributor.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const BecomeContributor = () => {
+  return (
+    <>
+      <Head>
+        <title>Become a contributor | Beautiful News</title>
+        <meta
+          name="description"
+          content="Join us in telling positive and uplifting stories. If you have content to contribute that could inspire the world, let us know below and we’ll be in touch."
+        />
+      </Head>
+      <Navbar />
+
+      <Footer />
+    </>
+  );
+};
+
+export default BecomcCntributor;
