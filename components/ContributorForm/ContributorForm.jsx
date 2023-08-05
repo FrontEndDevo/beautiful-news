1f8f64e024fd25c1249c1fdbfa198f2c3cec4d95
FrontEndDevo <mylinkacco@gmail.com>
2023-08-05 20:31:48 +0300

    Build the JSX of the form in ContributorForm component

--- components/ContributorForm/ContributorForm.jsx
+++ components/ContributorForm/ContributorForm.jsx
@@ -12,6 +12,75 @@ const ContributorForm = () => {
           </p>
         </div>
       </div>
+      <form className={classes.form}>
+        <div className={classes.inputs}>
+          <div className={classes.input}>
+            <input
+              type="text"
+              placeholder="My name Is*"
+              name="contributor-name"
+              id="contributor-name"
+            />
+          </div>
+          <div className={classes.input}>
+            <input
+              type="email"
+              placeholder="Email*"
+              name="contributor-email"
+              id="contributor-email"
+            />
+          </div>
+        </div>
+        <div className={classes.input}>
+          <input
+            type="text"
+            placeholder="Link to your portfolio or blog (optional)"
+            name="contributor-blog"
+            id="contributor-blog"
+          />
+        </div>
+        <div className={classes.input}>
+          <input
+            type="file"
+            placeholder="Upload photo (optional)"
+            name="upload-photo"
+            id="upload-photo"
+          />
+        </div>
+        <ul className={classes["I-want"]}>
+          <li>
+            <button>Submit a story idea</button>
+            <div className={classes.checking}>
+              <input type="checkbox" name="story-idea" id="story-idea" />
+              <span className={classes.checkmark}></span>
+            </div>
+          </li>
+          <li>
+            <button>Submit a story</button>
+            <div className={classes.checking}>
+              <input type="checkbox" name="Submit-story" id="Submit-story" />
+              <span className={classes.checkmark}></span>
+            </div>
+          </li>
+          <li>
+            <button>Edit a story</button>
+            <div className={classes.checking}>
+              <input type="checkbox" name="Edit-story" id="Edit-story" />
+              <span className={classes.checkmark}></span>
+            </div>
+          </li>
+        </ul>
+        <div className={classes.input}>
+          <textarea
+            name="message"
+            id="message"
+            rows="10"
+            placeholder="Additional message (optional)"
+            className={classes["additional message"]}
+          />
+        </div>
+        <button className={classes.submit}>Submit</button>
+      </form>
     </section>
   );
 };
