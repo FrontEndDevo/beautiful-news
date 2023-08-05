076b604cee94b8c2aed54dad601e43bc658fc329
FrontEndDevo <mylinkacco@gmail.com>
2023-08-05 20:14:16 +0300

    Add the titles of ContributorForm component and styling it

--- components/ContributorForm/ContributorForm.jsx
+++ components/ContributorForm/ContributorForm.jsx
@@ -1,5 +1,19 @@
+import classes from "./ContributorForm.module.scss";
 const ContributorForm = () => {
-  return <div>ContributorForm</div>;
+  return (
+    <section className={classes.contributor}>
+      <div className={classes.text}>
+        <div className={classes["contributor-title"]}>
+          <h2>Become a contributor</h2>
+          <p>
+            Join us in telling positive and uplifting stories. If you have
+            content to contribute that could inspire the world, let us know
+            below and we’ll be in touch.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ContributorForm;
