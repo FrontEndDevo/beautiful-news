af9ce805c1043be2ac2662130b2964c3229a90b0
FrontEndDevo <mylinkacco@gmail.com>
2024-03-22 00:44:45 +0200

    Replace account page with everything in the StickyNavbar component

--- components/StickyNavbar/StickyNavbar.jsx
+++ components/StickyNavbar/StickyNavbar.jsx
@@ -1,18 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./StickyNavbar.module.scss";
-import { faDisplay } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faDisplay, faRadio } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 const StickyNavbar = () => {
   return (
     <div className={classes["sticky-navbar"]}>
-      <Link href='/channels' className={classes.link}>
+      <Link href="/channels" className={classes.link}>
         <FontAwesomeIcon icon={faDisplay} />
         <span>Channels</span>
       </Link>
-      <Link href="/a-dynamic-name" className={classes.link}>
-        <FontAwesomeIcon icon={faUser} />
-        <span>Account</span>
+      <Link href="/everything" className={classes.link}>
+        <FontAwesomeIcon icon={faRadio} />
+        <span>Everything</span>
       </Link>
     </div>
   );
